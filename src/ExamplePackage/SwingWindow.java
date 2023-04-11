9eb7117dcbb68cd4ff50de8808892dff61383d62
majanik <majanik@10.67.40.21>
2023-04-11 15:32:12 +0200

    First swing window of the test program.

---
+++ src/ExamplePackage/SwingWindow.java
@@ -0,0 +1,21 @@
+package ExamplePackage;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class SwingWindow extends JFrame {
+
+	JButton buttonClose = new JButton("Close");
+	
+	SwingWindow()
+	{
+		this.setSize(500,500);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+		SwingWindow okno = new SwingWindow();
+		okno.setVisible(true);
+
+	}
+
+}
